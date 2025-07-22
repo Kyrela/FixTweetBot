a26edddd0b802ee102ab80f4b424a8cc5654cdda
Kyrela <mathis.vinay@gmail.com>
2025-07-22 22:03:03 +0200

    ♻️ setup: use wait_until_ready (since there isn't any guild chunking at startup anymore)

--- cogs/setup.py
+++ cogs/setup.py
@@ -22,18 +22,17 @@ class Setup(discore.Cog,
         if discore.config.topgg_token:
             self.topgg_client = topgg.DBLClient(discore.config.topgg_token)
 
-    @discore.Cog.listener()
-    async def on_connect(self):
-        if discore.config.analytic and not self.update_activity.is_running():
+    async def cog_load(self) -> None:
+        if discore.config.analytic:
             _logger.info("[ACTIVITY] Starting custom activity")
             self.update_activity.start()
-        elif not discore.config.analytic:
+        else:
             _logger.warning("[ACTIVITY] Analytics disabled, activity disabled")
 
-        if self.topgg_client and not self.topgg_autopost.is_running():
+        if self.topgg_client:
             _logger.info("[TOP.GG] Starting autopost")
             self.topgg_autopost.start()
-        elif not self.topgg_client:
+        else:
             _logger.warning("[TOP.GG] `config.topgg_token` not set, autopost disabled")
 
     async def cog_unload(self):
@@ -62,10 +61,6 @@ class Setup(discore.Cog,
         :return: None
         """
 
-        if self.bot.shard_count is None:
-            _logger.warning("[ACTIVITY] Websocket not connected, skipping activity update")
-            return
-
         fixed_links_nb = len(Event.since())
         if fixed_links_nb == 0:
             return
@@ -73,6 +68,10 @@ class Setup(discore.Cog,
         activity = discore.CustomActivity(f"Fixing {fixed_links_nb} links per day")
         _logger.info(f"[ACTIVITY] {activity}")
         await self.bot.change_presence(activity=activity)
+
+    @update_activity.before_loop
+    async def before_update_activity(self) -> None:
+        await self.bot.wait_until_ready()
 
     @discore.loop(hours=1)
     async def topgg_autopost(self) -> None:
@@ -82,10 +81,9 @@ class Setup(discore.Cog,
         :return: None
         """
 
-        guild_count = len(self.bot.guilds)
-        if guild_count == 0:
-            _logger.warning("[TOP.GG] No guilds found, skipping autopost")
-            return
-
-        await self.topgg_client.post_guild_count(guild_count=guild_count)
+        await self.topgg_client.post_guild_count(guild_count=len(self.bot.guilds))
         _logger.info("[TOP.GG] Updated guild count")
+
+    @topgg_autopost.before_loop
+    async def before_topgg_autopost(self) -> None:
+        await self.bot.wait_until_ready()
