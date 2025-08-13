429cc708a0d8526fef592d673bf3cbaa16a485e0
Kyrela <mathis.vinay@gmail.com>
2025-08-14 00:39:27 +0200

    ➖ intents: remove members

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from src.utils import I18nTranslator
 
 os.environ['DB_CONFIG_PATH'] = 'database/config.py'
 
-intents = discore.Intents(guild_messages=True, message_content=True, guilds=True, members=True)
+intents = discore.Intents(guild_messages=True, message_content=True, guilds=True)
 
 bot = discore.Bot(help_command=None, intents=intents)
 asyncio.run(bot.tree.set_translator(I18nTranslator()))
